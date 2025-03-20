c8f0161dda31f0c83cc3b6cb0e07a649637901d2
Kellen <kanarioskellen@gmail.com>
2025-03-20 13:41:26 -0400

    custom mcts policy + test

--- stoix/systems/gcrl/base/evaluator.py
+++ stoix/systems/gcrl/base/evaluator.py
@@ -1,7 +1,4 @@
-import functools
-import math
-import time
-from typing import Any, Dict, Optional, Tuple, Union
+from typing import Dict, Optional, Tuple, Union
 
 import chex
 import flax.linen as nn
@@ -48,160 +45,6 @@ def goal_act_fn(
         return action
 
     return act_fn
-
-
-def get_penalty(logits: chex.Array):
-    max = jnp.max(logits)
-
-    def penalty(val: chex.Array):
-        return jnp.log(1 - val / max)
-
-    vmap_penalty = jax.vmap(penalty, in_axes=(0))
-    return vmap_penalty(logits)
-
-
-def obstacle_act_fn(
-    config: DictConfig,
-    actor_apply: ActorApply,
-    rngs: Optional[Dict[str, chex.PRNGKey]] = None,
-) -> ObstActFn:
-    """Get the act_fn for a network that returns a distribution."""
-
-    def act_fn(
-        params: FrozenDict,
-        observation: chex.Array,
-        goal: chex.Array,
-        obstacles: chex.Array,
-        key: chex.PRNGKey,
-    ) -> chex.Array:
-        """Get the action from the distribution."""
-        # TODO: config.penalty_fn
-        vmap_obst = jax.vmap(actor_apply, in_axes=(None, None, 0))
-        obstacle_logits = vmap_obst(
-            params,
-            observation,
-            obstacles,
-        ).preferences
-        vmap_penalty = jax.vmap(get_penalty, in_axes=(0))
-        penalties = vmap_penalty(obstacle_logits)
-        penalty = jnp.mean(penalties, axis=0)
-
-        logits = actor_apply(params, observation, goal).preferences - penalty
-        action = jnp.argmax(logits)
-        return action
-
-    return act_fn
-
-
-def get_obst_evaluator_fn(
-    env: Environment,
-    act_fn: GoalActFn,
-    config: DictConfig,
-    log_solve_rate: bool = False,
-    eval_multiplier: int = 1,
-) -> EvalFn:
-    """Get the evaluator function for feedforward networks.
-
-    Args:
-        env (Environment): An environment instance for evaluation.
-        act_fn (callable): The act_fn that returns the action taken by the agent.
-        config (dict): Experiment configuration.
-        eval_multiplier (int): A scalar that will increase the number of evaluation
-            episodes by a fixed factor. The reason for the increase is to enable the
-            computation of the `absolute metric` which is a metric computed and the end
-            of training by rolling out the policy which obtained the greatest evaluation
-            performance during training for 10 times more episodes than were used at a
-            single evaluation step.
-    """
-
-    def eval_one_episode(params: FrozenDict, init_eval_state: EvalState) -> Dict:
-        """Evaluate one episode. It is vectorized over the number of evaluation episodes."""
-
-        def not_done(carry: Tuple) -> bool:
-            """Check if the episode is done."""
-            timestep = carry[2]
-            is_not_done: bool = ~timestep.last()
-            return is_not_done
-
-        def _obst_env_step(eval_state: EvalState) -> EvalState:
-            """Step the environment."""
-            # PRNG keys.
-            key, env_state, last_timestep, step_count, episode_return = eval_state
-
-            # Select action.
-            key, policy_key = jax.random.split(key)
-
-            action = act_fn(
-                params,
-                jax.tree_util.tree_map(
-                    lambda x: x[jnp.newaxis, ...],
-                    last_timestep.observation,
-                ),
-                last_timestep.extras["goal"][jnp.newaxis, ...],
-                last_timestep.extras["obstacles"][:, jnp.newaxis],
-                policy_key,
-            )
-
-            # Step environment.
-            env_state, timestep = env.step(env_state, action.squeeze())
-
-            # Log episode metrics.
-            episode_return += timestep.reward
-            step_count += 1
-            eval_state = EvalState(key, env_state, timestep, step_count, episode_return)
-            return eval_state
-
-        final_state = jax.lax.while_loop(not_done, _obst_env_step, init_eval_state)
-
-        eval_metrics = {
-            "episode_return": final_state.episode_return,
-            "episode_length": final_state.step_count,
-        }
-        # Log solve episode if solve rate is required.
-        if log_solve_rate:
-            eval_metrics["solve_episode"] = jnp.all(
-                final_state.episode_return >= config.env.solved_return_threshold
-            ).astype(int)
-
-        return eval_metrics
-
-    def evaluator_fn(trained_params: FrozenDict, key: chex.PRNGKey) -> EvaluationOutput[EvalState]:
-        """Evaluator function."""
-
-        # Initialise environment states and timesteps.
-        n_devices = len(jax.devices())
-
-        eval_batch = (config.arch.num_eval_episodes // n_devices) * eval_multiplier
-
-        key, *env_keys = jax.random.split(key, eval_batch + 1)
-        env_states, timesteps = jax.vmap(env.reset)(
-            jnp.stack(env_keys),
-        )
-        # Split keys for each core.
-        key, *step_keys = jax.random.split(key, eval_batch + 1)
-        # Add dimension to pmap over.
-        step_keys = jnp.stack(step_keys).reshape(eval_batch, -1)
-
-        eval_state = EvalState(
-            key=step_keys,
-            env_state=env_states,
-            timestep=timesteps,
-            step_count=jnp.zeros((eval_batch, 1)),
-            episode_return=jnp.zeros_like(timesteps.reward),
-        )
-
-        eval_metrics = jax.vmap(
-            eval_one_episode,
-            in_axes=(None, 0),
-            axis_name="eval_batch",
-        )(trained_params, eval_state)
-
-        return EvaluationOutput(
-            learner_state=eval_state,
-            episode_metrics=eval_metrics,
-        )
-
-    return evaluator_fn
 
 
 def get_goal_evaluator_fn(
@@ -320,7 +163,6 @@ def evaluator_setup(
     eval_act_fn: Union[ActFn, RecActFn, GoalActFn, ObstActFn],
     params: FrozenDict,
     config: DictConfig,
-    obstacle: bool = True,
 ) -> Tuple[EvalFn, EvalFn, Tuple[FrozenDict, chex.Array]]:
     """Initialise evaluator_fn."""
     # Get available TPU cores.
@@ -331,7 +173,7 @@ def evaluator_setup(
     else:
         log_solve_rate = False
 
-    get_evaluator_fn = get_obst_evaluator_fn if obstacle else get_goal_evaluator_fn
+    get_evaluator_fn = get_goal_evaluator_fn
     evaluator = get_evaluator_fn(eval_env, eval_act_fn, config, log_solve_rate)  # type: ignore
 
     absolute_metric_evaluator = get_evaluator_fn(
---
+++ stoix/systems/gcrl/search/mcts_policy.py
@@ -0,0 +1,130 @@
+import functools
+from typing import Any, Optional
+
+import chex
+import jax
+import jax.numpy as jnp
+import mctx
+import mctx._src.qtransforms as qtransforms
+from flax.core import FrozenDict
+from mctx import search
+from mctx._src import tree as tree_lib
+from mctx._src.base import PolicyOutput, QTransform
+
+Params = Any
+
+
+def _get_logits_from_probs(probs):
+    tiny = jnp.finfo(probs.dtype).tiny
+    return jnp.log(jnp.maximum(probs, tiny))
+
+
+def _apply_temperature(logits, temperature):
+    """Returns `logits / temperature`, supporting also temperature=0."""
+    # The max subtraction prevents +inf after dividing by a small temperature.
+    logits = logits - jnp.max(logits, keepdims=True, axis=-1)
+    tiny = jnp.finfo(logits.dtype).tiny
+    return logits / jnp.maximum(tiny, temperature)
+
+
+def action_selection(
+    rng_key: chex.PRNGKey,
+    tree: tree_lib.Tree,
+    node_index: chex.Numeric,
+    depth: chex.Numeric,
+    *,
+    qtransform: QTransform = qtransforms.qtransform_by_parent_and_siblings,
+) -> chex.Array:
+    visit_counts = tree.children_visits[node_index]
+    node_visit = tree.node_visits[node_index]
+    # pb_c = pb_c_init + jnp.log((node_visit + pb_c_base + 1.0) / pb_c_base)
+    prior_logits = tree.children_prior_logits[node_index]
+    prior_probs = jax.nn.softmax(prior_logits)
+    # policy_score = jnp.sqrt(node_visit) * pb_c * prior_probs / (visit_counts + 1)
+    policy_score = jnp.sqrt(node_visit) * prior_probs / (visit_counts + 1)
+    chex.assert_shape([node_index, node_visit], ())
+    chex.assert_equal_shape([prior_probs, visit_counts, policy_score])
+    value_score = qtransform(tree, node_index)
+
+    # Add tiny bit of randomness for tie break
+    node_noise_score = 1e-7 * jax.random.uniform(rng_key, (tree.num_actions,))
+    to_argmax = value_score + policy_score + node_noise_score
+
+    # Masking the invalid actions at the root.
+    return jnp.argmax(to_argmax)
+
+
+def safe_policy(
+    params: Params,
+    rng_key: chex.PRNGKey,
+    root: mctx.RootFnOutput,
+    recurrent_fn: mctx.RecurrentFn,
+    num_simulations: int,
+    invalid_actions: Optional[chex.Array] = None,
+    max_depth: Optional[int] = None,
+    loop_fn: mctx.LoopFn = jax.lax.fori_loop,
+    *,
+    qtransform: QTransform = qtransforms.qtransform_by_parent_and_siblings,
+    temperature: chex.Numeric = 1.0,
+) -> mctx.PolicyOutput[None]:
+    """Runs MuZero search and returns the `PolicyOutput`.
+
+    In the shape descriptions, `B` denotes the batch dimension.
+
+    Args:
+      params: params to be forwarded to root and recurrent functions.
+      rng_key: random number generator state, the key is consumed.
+      root: a `(prior_logits, value, embedding)` `RootFnOutput`. The
+        `prior_logits` are from a policy network. The shapes are
+        `([B, num_actions], [B], [B, ...])`, respectively.
+      recurrent_fn: a callable to be called on the leaf nodes and unvisited
+        actions retrieved by the simulation step, which takes as args
+        `(params, rng_key, action, embedding)` and returns a `RecurrentFnOutput`
+        and the new state embedding. The `rng_key` argument is consumed.
+      num_simulations: the number of simulations.
+      invalid_actions: a mask with invalid actions. Invalid actions
+        have ones, valid actions have zeros in the mask. Shape `[B, num_actions]`.
+      max_depth: maximum search tree depth allowed during simulation.
+      loop_fn: Function used to run the simulations. It may be required to pass
+        hk.fori_loop if using this function inside a Haiku module.
+      qtransform: function to obtain completed Q-values for a node.
+      temperature: temperature for acting proportionally to
+        `visit_counts**(1 / temperature)`.
+
+    Returns:
+      `PolicyOutput` containing the proposed action, action_weights and the used
+      search tree.
+    """
+    rng_key, dirichlet_rng_key, search_rng_key = jax.random.split(rng_key, 3)
+
+    # Adding Dirichlet noise.
+    prior_logits = jax.nn.softmax(root.prior_logits)
+    # root = root.replace(prior_logits=_mask_invalid_actions(noisy_logits, invalid_actions))
+    root = root.replace(prior_logits=prior_logits)
+
+    # Running the search.
+    interior_action_selection_fn = functools.partial(
+        action_selection,
+        qtransform=qtransform,
+    )
+    root_action_selection_fn = functools.partial(interior_action_selection_fn, depth=0)
+
+    search_tree = search(
+        params=params,
+        rng_key=search_rng_key,
+        root=root,
+        recurrent_fn=recurrent_fn,
+        root_action_selection_fn=root_action_selection_fn,
+        interior_action_selection_fn=interior_action_selection_fn,
+        num_simulations=num_simulations,
+        max_depth=max_depth,
+        invalid_actions=invalid_actions,
+        loop_fn=loop_fn,
+    )
+
+    # Sampling the proposed action proportionally to the visit counts.
+    summary = search_tree.summary()
+    action_weights = summary.visit_probs
+    action_logits = _apply_temperature(_get_logits_from_probs(action_weights), temperature)
+    action = jax.random.categorical(rng_key, action_logits)
+    return PolicyOutput(action=action, action_weights=action_weights, search_tree=search_tree)
